d030055689681d9eddd9a0bff92f72dbf731a366
Odd-Harald Lillestø Myhren <ohmyhren@uia.no>
2021-10-12 00:12:51 +0200

    basic styling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use std::{
+    fmt::Write,
     fs::read_dir,
     io::{stdin, stdout},
     path::PathBuf,
 };
 use termion::{
+    color,
     event::Key,
     input::{MouseTerminal, TermRead},
     raw::IntoRawMode,
@@ -12,12 +14,15 @@ use termion::{
 };
 use tui::{
     backend::TermionBackend,
-    text::Span,
-    widgets::{Block, Borders, Paragraph},
+    style::{Color, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, BorderType, Borders, Paragraph},
     Terminal,
 };
 
+#[derive(Default)]
 struct Context {
+    cursor: usize,
     current_dir: PathBuf,
 }
 
@@ -25,6 +30,7 @@ impl Context {
     fn new() -> Result<Self> {
         let context = Self {
             current_dir: std::env::current_dir()?,
+            ..Self::default()
         };
         Ok(context)
     }
@@ -33,15 +39,28 @@ impl Context {
         self.current_dir.as_os_str().to_str()
     }
 
-    fn listing(&self) -> Result<String> {
-        let mut output = String::new();
-        for path in read_dir(&self.current_dir)? {
-            if let Some(input) = path?.file_name().to_str() {
-                output.push_str(input);
-                output.push('\n');
+    fn listing(&self) -> Result<Text<'_>> {
+        let mut text = Text::default();
+        for (line, path) in read_dir(&self.current_dir)?.enumerate() {
+            let path = path?;
+            if let Some(input) = path.file_name().to_str() {
+                let input = input.to_string();
+                let mut spans = Spans::default();
+                if self.cursor == line {
+                    spans.0.push(Span::styled(
+                        input,
+                        Style::default().fg(Color::Black).bg(Color::White),
+                    ));
+                } else {
+                    spans.0.push(Span::raw(input));
+                }
+                if path.path().is_dir() {
+                    spans.0.push(Span::raw("/"));
+                }
+                text.lines.push(spans);
             }
         }
-        Ok(output)
+        Ok(text)
     }
 }
 
@@ -60,7 +79,9 @@ fn main() -> Result<()> {
 
             // Files pane
             let directory = Span::from(context.current_dir().unwrap_or("Invalid directory"));
-            let outline = Block::default().borders(Borders::ALL).title(directory);
+            let outline = Block::default()
+                .borders(Borders::ALL)
+                .title(directory);
             let files = Paragraph::new(listing).block(outline);
             frame.render_widget(files, size);
         })?;
