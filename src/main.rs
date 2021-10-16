73c2b65f43a41456d7d90328e414f873b7e7ce68
Odd-Harald Lillestø Myhren <ohmyhren@uia.no>
2021-10-16 03:10:28 +0200

    added views

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,128 @@ use tui::{
     Terminal,
 };
 
+#[derive(Default)]
+struct Views {
+    index: usize,
+    contexts: [Context; 4],
+}
+
+impl Views {
+    fn new() -> Result<Self> {
+        let mut views = Self::default(); 
+        for context in &mut views.contexts {
+            *context = Context::new()?;
+        }
+        Ok(views)
+    }
+
+    fn current_context(&mut self) -> &mut Context {
+        &mut self.contexts[self.index]
+    }
+}
+
+fn main() -> Result<()> {
+    let stdout = stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    let mut views = Views::new()?;
+
+    'update: loop {
+        // Assign current context
+        let mut context = views.current_context();
+
+        // Clamp the cursor if it's out of bounds
+        let amount_files = context.read_directory()?.count();
+        if context.cursor >= amount_files {
+            context.cursor = amount_files.saturating_sub(1);
+        }
+
+        let listing = context.listing()?;
+
+        terminal.draw(|frame| {
+            let size = frame.size();
+
+            // Files pane
+            let directory = Span::from(context.current_dir().unwrap_or("Invalid directory"));
+            let outline = Block::default()
+                .title(directory)
+                .style(Style::default().fg(Color::LightGreen));
+            let files = Paragraph::new(listing).block(outline);
+            frame.render_widget(files, size);
+        })?;
+
+        for key in stdin().keys() {
+            if let Ok(key) = key {
+                match key {
+                    Key::Char('q') | Key::Ctrl('c') | Key::Ctrl('z') => break 'update,
+                    Key::Up | Key::Char('k') => {
+                        context.cursor = context.cursor.saturating_sub(1);
+                    }
+                    Key::Down | Key::Char('j') => {
+                        context.cursor = context.cursor.saturating_add(1);
+                    }
+                    Key::Left | Key::Char('h') => {
+                        context.save_location();
+                        context.current_dir.pop();
+                        context.restore_location();
+                    }
+                    Key::Right | Key::Char('l') => {
+                        if let Ok(target) = context.target_dir() {
+                            context.save_location();
+                            context.current_dir.push(target);
+                            context.restore_location();
+                        }
+                    }
+                    Key::Char('.') => {
+                        context.options.show_hidden = !context.options.show_hidden;
+                    }
+                    Key::Char('e') => {
+                        if let Some(target) = context.target() {
+                            if edit_this::file(target.path()).is_err() {
+                                // Output error to log
+                            }
+                            terminal.clear()?;
+                        }
+                    }
+                    Key::Ctrl('d') => {
+                        context.cursor = context.cursor.saturating_add(10);
+                    }
+                    Key::Ctrl('u') => {
+                        context.cursor = context.cursor.saturating_sub(10);
+                    }
+                    Key::Char('o') => {
+                        if let Some(target) = context.target() {
+                            if open::that(target.path()).is_err() {
+                                // Output error to log
+                            }
+                        }
+                    }
+                    Key::Char(index) if ('1'..'4').any(|digit| digit == index) => {
+                        if let Some(index) = index.to_digit(10) {
+                            views.index = index.saturating_sub(1) as usize;
+                        }
+                    }
+                    _ => {}
+                }
+                continue 'update;
+            }
+        }
+    }
+
+    // Fix wonkyness
+    terminal.clear()?;
+
+    // Write last entered directory into temporary file
+    if let Some(mut path) = dirs::cache_dir() {
+        path.push(".rrr");
+        let mut file = File::create(path)?;
+        write!(&mut file, "{}", views.current_context().current_dir.display())?;
+    }
+
+    Ok(())
+}
+
 enum Target {
     File,
     Directory,
@@ -139,95 +261,4 @@ impl Context {
         }
         Ok(text)
     }
-}
-
-fn main() -> Result<()> {
-    let stdout = stdout().into_raw_mode()?;
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    let mut context = Context::new()?;
-
-    'update: loop {
-        // Clamp the cursor if it's out of bounds
-        let amount_files = context.read_directory()?.count();
-        if context.cursor >= amount_files {
-            context.cursor = amount_files.saturating_sub(1);
-        }
-
-        let listing = context.listing()?;
-
-        terminal.draw(|frame| {
-            let size = frame.size();
-
-            // Files pane
-            let directory = Span::from(context.current_dir().unwrap_or("Invalid directory"));
-            let outline = Block::default()
-                .title(directory)
-                .style(Style::default().fg(Color::LightGreen));
-            let files = Paragraph::new(listing).block(outline);
-            frame.render_widget(files, size);
-        })?;
-
-        for key in stdin().keys() {
-            if let Ok(key) = key {
-                match key {
-                    Key::Char('q') | Key::Ctrl('c') | Key::Ctrl('z') => break 'update,
-                    Key::Up | Key::Char('k') => {
-                        context.cursor = context.cursor.saturating_sub(1);
-                    }
-                    Key::Down | Key::Char('j') => {
-                        context.cursor = context.cursor.saturating_add(1);
-                    }
-                    Key::Left | Key::Char('h') => {
-                        context.save_location();
-                        context.current_dir.pop();
-                        context.restore_location();
-                    }
-                    Key::Right | Key::Char('l') => {
-                        if let Ok(target) = context.target_dir() {
-                            context.save_location();
-                            context.current_dir.push(target);
-                            context.restore_location();
-                        }
-                    }
-                    Key::Char('.') => {
-                        context.options.show_hidden = !context.options.show_hidden;
-                    }
-                    Key::Char('e') => {
-                        if let Some(target) = context.target() {
-                            if edit_this::file(target.path()).is_err() {
-                                // Output error to log
-                            }
-                            terminal.clear()?;
-                        }
-                    }
-                    Key::Ctrl('d') => {
-                        context.cursor = context.cursor.saturating_add(10);
-                    }
-                    Key::Ctrl('u') => {
-                        context.cursor = context.cursor.saturating_sub(10);
-                    }
-                    Key::Char('o') => {
-                        if let Some(target) = context.target() {
-                            if open::that(target.path()).is_err() {
-                                // Output error to log
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-                continue 'update;
-            }
-        }
-    }
-
-    // Write last entered directory into temporary file
-    if let Some(mut path) = dirs::cache_dir() {
-        path.push(".rrr");
-        let mut file = File::create(path)?;
-        write!(&mut file, "{}", context.current_dir.display())?;
-    }
-
-    Ok(())
 }
